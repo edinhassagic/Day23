7a9ab7314cc8996b114015b924c7f98ba777d275
edin hasagic <edinhassagic@gmail.com>
2021-03-30 15:38:32 +0200

    1

---
+++ ex01/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Plant from './Plant';
+
+function App() {
+ 
+  return (
+    <div id="App">
+      <Plant />
+    </div>
+  );
+  
+}
+
+export default App;
+
+
+
---
+++ ex01/src/Plant.js
@@ -0,0 +1,15 @@
+import React, {Component}  from 'react';
+
+class Plant extends React.Component {
+constructor(props) {
+    super(props);
+    this.state = { type: "Flowering plant" };
+}
+render () {
+    return ( <div>
+        <h2>This is a Plant Component!</h2>
+    </div> 
+    );
+}
+}
+export default Plant;
